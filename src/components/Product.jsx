45aa3a23d36d5394caa32f0867ea01183dfd2a11
TeancumScotson <teancumscotson@developersinstitute.co.nz>
2023-06-21 13:31:52 +1200

    finished all styling
    updated mobile styling and finished tablet and desktop

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,10 +2,10 @@ const Product = ({ name, description, src, price, onCheckout }) => {
   return (
     <li className="product-grid-item">
       <img className="image"src={src} alt={name}/>
-      <p>${price}</p>
-      <h3>{name}</h3>
-      <p>{description}</p>
-      <button onClick={onCheckout}>Buy now</button>
+      <p className="price">${price}.00</p>
+      <h3 className="name">{name}</h3>
+      <p className="desc">{description}</p>
+      <button className="buybutton" onClick={onCheckout}>Buy now</button>
     </li>
   );
 };
